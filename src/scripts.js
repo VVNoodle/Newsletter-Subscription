e0b45ae5b5f479ff9fcffe562b58ea8d30c8dd28
Egan <ebisma@ucsc.edu>
2017-11-12 20:27:58 -0800

    Using ajax and jquery, we can now request the server without reloading the page

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,17 @@
-console.log("Test test test");
+const $ = require("jquery");
+
+$('form').submit((event) => {
+    event.preventDefault();
+    console.log("event called");
+    $.ajax({
+        url: '/',
+        type: 'POST',
+        data: {
+            email: 'ebisma@ucsc.edu'
+        },
+        success: (response) => {
+            console.log(response);
+        }
+    });
+});
+
